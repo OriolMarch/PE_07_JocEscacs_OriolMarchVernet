04e253db66bbed1e0a8ca3b14cc31c22491f1c68
oriolmarch <icewoo2001@gmail.com>
2026-01-12 16:43:13 +0100

    first commit

---
+++ PE_07_OriolMarchVernet.java
@@ -0,0 +1,10 @@
+public class PE_07_OriolMarchVernet {
+    public static void main(String[] args) {
+    PE_07_OriolMarchVernet p = new PE_07_OriolMarchVernet();
+    p.main();
+    }
+
+    public void main() {
+       
+    }
+}
